07ac1fab43d7cb825af99fca0d2c908591c14afb
Vanessa Schneider <vanessajwade@gmail.com>
2022-04-20 14:17:48 -0400

    fixedstuff

--- client/src/App.js
+++ client/src/App.js
@@ -87,7 +87,7 @@ function App() {
       {user ? null : <Signup onLogin={setUser} login={login} /> }
        {user? null  :<h1> Welcome to Kingdom Quest!</h1>}
       <NavBar taskCompleted = {taskCompleted} setTaskCompleted={setTaskCompleted} user={user}/>
-      {user ? null : <img src = {Welcome} className = "size"></img>}
+      {user ? null : <img src = {Welcome} className = "welcome"></img>}
       {user && user.username ? <Home/> : null}
     
 
--- client/src/Warlock.js
+++ client/src/Warlock.js
@@ -57,14 +57,14 @@ return(
 {Play()}
 {userWins > 4 && task.task5 ===1 ? null :<RockContainer task={task} userWins={userWins} setUserWins={setUserWins} user={user} UserWins={UserWins} />}
 {userWins > 4  && wTalks2 === 0 ? <img src = {warlock} className = "size" alt = "image"></img> : null}
-{userWins > 4 && wTalks2 ===0  ? <div> <p> I can't believe you were able to win 5 games. I surrender. I will give King Chicken the crown and change out of my scary robe and take out my red contacts.</p>
-<button className = "buttons" onClick = {increment} >Say, "Yes"</button> </div> : null}
+{userWins > 4 && wTalks2 ===0  ? <div> <h3 className = "riddle-correct"> I can't believe you were able to win 5 games. I surrender. I will give King Chicken the crown and change out of my scary robe and take out my red contacts.</h3>
+<button className = "the-button" onClick = {increment} >Say, "Yes"</button> </div> : null}
 {userWins > 4 && wTalks2 ===1  ? <div> <img src = {teddy} className = "size" alt = "image"></img>
 <p> Yes, I really was just a teddy bear in a scary cloak. But now we can all live happily ever after!</p>
 <h2> The End!</h2></div>: null}
 {task.task5 ===1 ? <div> <img src = {teddy} className = "size" alt = "image"></img> <div className = "riddle-correct">
-<p> Yes, I really was just a teddy bear in a scary cloak. But now we can all live happily ever after!</p>
-<h2> The End!</h2></div></div>: null}
+<h3 className = "riddle-correct"> Yes, I really was just a teddy bear in a scary cloak. But now we can all live happily ever after!</h3>
+<h2 className = "the-end"> The End!</h2></div></div>: null}
 
 </div>
 
